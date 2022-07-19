d3e32b8628ba522538d8cc2fb2102a45ed9456b2
aqimuxu <qinying1130@163.com>
2022-07-19 18:43:14 +0800

    详情页-用模板渲染从后台得到的页面数据并在浏览器打开

--- mytest/30 detailpage/nodejs-client/index.js
+++ mytest/30 detailpage/nodejs-client/index.js
@@ -1,17 +1,25 @@
 const mount = require('koa-mount');
 const static = require('koa-static');
 const app = new (require('koa'));
+const rpcClient = require('./client');
+const template = require('./template')
 
+const detailTemplate = template(__dirname+'/template/index.html')
 app.use(mount('/static', static(`${__dirname}/source/static`)))
 
 app.use(async (ctx) => {
+    const result = await new Promise((resolve, reject)=>{
+        rpcClient.write({
+            columnid:ctx.query.columnid
+        },function (err,data){
+            err? reject(err):resolve(data)
+        })
+    })
+    ctx.status = 200
+
+    ctx.body = detailTemplate(result)
 
 })
 
-// app.listen(3000)
-const rpcClient = require('./client');
-rpcClient.write({
-    columnid:24
-},function (err,data){
-    console.log(err,data)
-})
+app.listen(3000)
+
